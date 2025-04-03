03d4dcc27fb5b3af0743df20e2e40cc6bdb1bfa5
Tomas Moya Pavlatka <tomas@pavlatka.cz>
2025-04-03 06:03:27 +0200

    init commit.

---
+++ src/test/java/cz/tomaspavlatka/spring_chef/SpringChefApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.tomaspavlatka.spring_chef;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringChefApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
