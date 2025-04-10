8b45293927ed63aa890624a902e572440248b8de
Tomas Moya Pavlatka <tomas@pavlatka.cz>
2025-04-10 17:49:25 +0200

    better presenter

--- src/main/java/cz/tomaspavlatka/spring/chef/command/LeadBillingsCommand.java
+++ src/main/java/cz/tomaspavlatka/spring/chef/command/LeadBillingsCommand.java
@@ -7,24 +7,26 @@ import org.springframework.shell.command.annotation.Option;
 
 import cz.tomaspavlatka.spring.chef.lead.usecase.query.GetBillingsSummaryQuery;
 
-@Command(group = "Billings", command = "billings")
-public class BillingsCommand {
+@Command(group = "Leads", command = "leads")
+public class LeadBillingsCommand {
   @Autowired
   private Terminal terminal;
 
   private final GetBillingsSummaryQuery getBillingsSummaryQuery;
 
-  public BillingsCommand(
+  public LeadBillingsCommand(
       GetBillingsSummaryQuery getBillingsSummaryQuery) {
     this.getBillingsSummaryQuery = getBillingsSummaryQuery;
   }
 
-  @Command(command = "summary", description = "Shows summary for specific month")
+  @Command(command = "billing-summary", description = "Shows lead billing summary for specific month")
   void summary(@Option(required = true) Integer year, @Option(required = true) Integer month) {
     var summary = getBillingsSummaryQuery.getSummary(year, month);
 
-    terminal.writer().println("Billings Summary, Y:" + year + ", M:" + month);
-    terminal.writer().println("===============================");
+    terminal.writer().println("-----------------");
+    terminal.writer().println("| Lead Billings |");
+    terminal.writer().println("-----------------");
+    terminal.writer().println("SUMMARY, Y:" + year + ", M:" + month);
 
     terminal.writer().println("- Quantities");
     summary.quantities().forEach((key, value) -> {
